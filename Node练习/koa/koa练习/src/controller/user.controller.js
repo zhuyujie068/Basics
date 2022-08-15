50ef4f44b1eda9a335f60bad1ae68f9e6e4d526f
zyj <1041586971@qq.com>
2022-08-15 15:12:18 +0800

    feat：[koa练习] 新增 user controller

--- Node练习/koa/koa练习/src/controller/user.controller.js
+++ Node练习/koa/koa练习/src/controller/user.controller.js
@@ -1,8 +1,25 @@
 // user router 控制器 ， 用来存放 方法
 
+// 导入 user 操作 sql 逻辑，可不再拆分出去，直接写在该文件中
+const { createUser } = require('../service/user.service')
+
+
 class UserController {
+  // 注册
   async register(ctx,next) {
-    ctx.body = '用户注册成功'
+    // 1.获取数据
+    const { user_name, password } = ctx.request.body;
+
+    // 2.操作数据库
+    const res = await createUser(user_name, password)
+
+    // 3. 返回结果
+    ctx.body = ctx.request.body
+  }
+
+  // 登录
+  async login(ctx,next) {
+    ctx.body = '登录成功'
   }
 }
 
