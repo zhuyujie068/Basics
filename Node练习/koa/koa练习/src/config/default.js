a59988270bcf8710fe039b6d4912e1c0baa6567d
zyj <1041586971@qq.com>
2022-08-26 11:09:52 +0800

    feat：[雇员管理] 修改 table 显示

--- Node练习/koa/koa练习/src/config/default.js
+++ Node练习/koa/koa练习/src/config/default.js
@@ -1,5 +1,7 @@
-const dotenv = require('dotenv');
+// 引入 dotenv
+const dotenv = require('dotenv'); 
 
-dotenv.config()
+// 将 .env 文件内容加载到 process.env 中
+dotenv.config(); 
 
 module.exports = process.env
