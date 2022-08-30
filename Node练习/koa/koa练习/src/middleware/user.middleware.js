95af87c4c4ef752e696ad2da4cb2079ecb53c583
zyj <1041586971@qq.com>
2022-08-30 15:16:48 +0800

    feat：[koa练习] 使用 bcryptjs 进行密码加密

--- Node练习/koa/koa练习/src/middleware/user.middleware.js
+++ Node练习/koa/koa练习/src/middleware/user.middleware.js
@@ -1,5 +1,8 @@
 // 拆分一个中间件层, 封装多个中间件函数，进行检验 (可以不拆，直接在 user.controller 中进行判断)
 
+// 导入 bcrypt 进行密码加密
+const bcrypt = require("bcryptjs");
+
 const { getUerInfo } = require("../service/user.service");
 
 // 导入错误类型
@@ -38,7 +41,21 @@ const verifyUser = async (ctx, next) => {
   await next();
 };
 
+// 将密码进行 bcrypt 加密
+const crpytPassword = async (ctx, next) => {
+  const { password } = ctx.request.body;
+
+  const salt = bcrypt.genSaltSync(10);
+
+  // hash保存的是 密文
+  const hash = bcrypt.hashSync(password, salt);
+  ctx.request.body.password = hash;
+
+  await next();
+};
+
 module.exports = {
   userValidator,
   verifyUser,
+  crpytPassword,
 };
--- Node练习/koa/koa练习/src/router/user.router.js
+++ Node练习/koa/koa练习/src/router/user.router.js
@@ -4,14 +4,14 @@
 const Router = require("koa-router");
 
 const { register, login } = require("../controller/user.controller");
-const { userValidator, verifyUser } = require("../middleware/user.middleware");
+const { userValidator, verifyUser, crpytPassword } = require("../middleware/user.middleware");
 
 // prefix 加前缀
 const router = new Router({ prefix: "/users" }); // 创建路由，支持传递参数
 
 // GET/POST: 加前缀 + 路径   /users/index
 router
-  .post("/register", userValidator, verifyUser, register) // 注册,先通过 userValidator、verifyUser 中间件 进行检验，检验通过再进行注册操作
+  .post("/register", userValidator, verifyUser, crpytPassword, register) // 注册,先通过 userValidator、verifyUser 中间件 进行检验，检验通过再进行注册操作
   .post("/login", login) // 登录
   .get("/index", (ctx, next) => {
     ctx.body = "hi users index";
