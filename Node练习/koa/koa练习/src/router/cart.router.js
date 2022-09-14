5b1ba8ea4fae8b33c27871f8addd3a8e4d997954
zyj <1041586971@qq.com>
2022-09-14 17:45:05 +0800

    fix：[koa练习] 修改 购物车 router 文件

--- Node练习/koa/koa练习/src/router/cart.router.js
+++ Node练习/koa/koa练习/src/router/cart.router.js
@@ -1,15 +1,15 @@
 // 1.导入koa-router
 const Router = require("koa-router");
 
+// 2.实例化router对象
+const router = new Router({ prefix: "/carts" });
+
 // 中间件
 const { auth } = require("../middleware/auth.middleware");
 const { validator } = require("../middleware/cart.middleware");
 
 // 控制器
 const { add, findAll, update, remove, selectAll, unselectAll } = require("../controller/cart.controller");
-
-// 2.实例化router对象
-const router = new Router({ prefix: "/carts" });
 
 // 3.编写路由规则
 router.post("/", auth, validator({ goods_id: "number" }), add); //添加到购物车
